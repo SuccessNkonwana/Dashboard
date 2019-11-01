a93ce464633f1e122e760f84492202d06bca0c48
Beverley Komane <andanimatoro@gmail.com>
2019-11-01 15:19:57 +0200

    display

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -111,56 +111,188 @@ export class HomeComponent implements OnInit {
   mb: number = 0; fb: number = 0; ob: number = 0;
 
   ngOnInit() {
+  this.char()
+  }
+  char(){
     this.firestore.collection(`localCafe`).valueChanges()
-      .subscribe(itemList => {
-        this.itemList = itemList;
+    .subscribe(itemList => {
+      this.itemList = itemList;
 
+    });
+
+  // this.users = this.firestore.collection('users').valueChanges();
+  // console.log(this.users)
+
+
+  this.firestore.collection('users').valueChanges().subscribe((data: any) => {
+    //  console.log(data)
+    this.f = [{ data }]
+    //  console.log(this.f[0].data[0]);
+    for (let i = 0; i < data.length; i++) {
+      let gender = data[i].gender;
+      let race = data[i].ethnicity;
+
+      // calculations for race
+      if (race == 'Black') {
+        this.b = this.b + 1
+
+      } else if (race == 'White') {
+        this.w = this.w + 1
+
+      }
+      else {
+        this.c = this.c + 1
+
+      }
+      // calculation for gender
+      if (gender == 'male') {
+        this.boy = this.boy + 1
+        // console.log(this.boy)
+      } else if (gender == 'female') {
+        this.girl = this.girl + 1
+        // console.log(this.girl)
+      }
+      else {
+        this.other = this.other + 1
+        // console.log(this.other)
+      }
+
+      this.chart = new Chart('chart', {
+        type: 'pie',
+        data: {
+          labels: ['Black', 'White', 'Coloured'],
+          datasets: [{
+            label: '# Ethnicity statistics',
+            data: [this.b, this.w, this.c],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        // options: {
+        //     scales: {
+        //         yAxes: [{
+        //             ticks: {
+        //                 beginAtZero: true
+        //             }
+        //         }]
+        //     }
+        // }
+      });
+      // graph 2
+      this.chart = new Chart('chart2', {
+        type: 'bar',
+        data: {
+
+          labels: ['Male', 'Female', 'Other'],
+          datasets: [{
+
+            label: '# Gender statistics',
+            data: [this.boy, this.girl, this.other],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              barThickness: 80,
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
       });
 
-    // this.users = this.firestore.collection('users').valueChanges();
-    // console.log(this.users)
-
-
-    this.firestore.collection('users').valueChanges().subscribe((data: any) => {
-      //  console.log(data)
-      this.f = [{ data }]
-      //  console.log(this.f[0].data[0]);
+    }
+    // graph 3
+    this.firestore.collection('localCafe').valueChanges().subscribe((data: any) => {
+      // this.f=[{data}]
+      //  console.log(data);
       for (let i = 0; i < data.length; i++) {
-        let gender = data[i].gender;
-        let race = data[i].ethnicity;
-
-        // calculations for race
-        if (race == 'Black') {
-          this.b = this.b + 1
-
-        } else if (race == 'White') {
-          this.w = this.w + 1
-
-        }
-        else {
-          this.c = this.c + 1
-
-        }
+        let d = data[i].user.gender;
+        let e = data[i].user.ethnicity;
+        // console.log(d)
         // calculation for gender
-        if (gender == 'male') {
-          this.boy = this.boy + 1
-          // console.log(this.boy)
-        } else if (gender == 'female') {
-          this.girl = this.girl + 1
-          // console.log(this.girl)
+        if (d == 'male' && e == 'White') {
+          this.mw = this.mw + 1
+          console.log("male white:" + this.mw)
+        } else if (d == 'male' && e == 'Coloured') {
+          this.mc = this.mc + 1
+          console.log("male coloured:" + this.mc)
+        } else if (d == 'male' && e == 'Black') {
+          this.mb = this.mb + 1
+          console.log("maleblack:" + this.mb)
         }
-        else {
-          this.other = this.other + 1
-          // console.log(this.other)
+        // females
+        if (d == 'female' && e == 'White') {
+          this.fw = this.fw + 1
+          console.log("f white:" + this.fw)
+        } else if (d == 'female' && e == 'Coloured') {
+          this.fc = this.fc + 1
+          console.log("f coloured:" + this.fc)
+        } else if (d == 'female' && e == 'Black') {
+          this.fb = this.fb + 1
+          console.log("f black:" + this.fb)
         }
-
-        this.chart = new Chart('chart', {
-          type: 'pie',
+        // graph
+        this.chart = new Chart('chart3', {
+          type: 'line',
           data: {
+
             labels: ['Black', 'White', 'Coloured'],
             datasets: [{
-              label: '# Ethnicity statistics',
-              data: [this.b, this.w, this.c],
+              // males
+              label: '# Male statistics per race',
+              data: [this.mb, this.mw, this.mc],
+
+              borderColor: [
+                // 'rgba(255, 99, 132, 1)',
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                'rgba(0,0,255, 1)'
+                // rgb(0,0,255)
+              ],
+              borderWidth: 2
+            },
+            {
+              // females
+              label: '# female statistics per race',
+              data: [this.fb, this.fw, this.fc],
               backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -170,61 +302,22 @@ export class HomeComponent implements OnInit {
                 'rgba(255, 159, 64, 0.2)'
               ],
               borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)'
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
               ],
-              borderWidth: 1
-            }]
-          },
-          // options: {
-          //     scales: {
-          //         yAxes: [{
-          //             ticks: {
-          //                 beginAtZero: true
-          //             }
-          //         }]
-          //     }
-          // }
-        });
-        // graph 2
-        this.chart = new Chart('chart2', {
-          type: 'bar',
-          data: {
+              borderWidth: 2
+            }
 
-            labels: ['Male', 'Female', 'Other'],
-            datasets: [{
+            ],
 
-              label: '# Gender statistics',
-              data: [this.boy, this.girl, this.other],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-              ],
-
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1,
-
-            }]
           },
           options: {
             scales: {
               yAxes: [{
-                barThickness: 80,
                 ticks: {
                   beginAtZero: true
                 }
@@ -234,102 +327,12 @@ export class HomeComponent implements OnInit {
         });
 
       }
-      // graph 3
-      this.firestore.collection('localCafe').valueChanges().subscribe((data: any) => {
-        // this.f=[{data}]
-        //  console.log(data);
-        for (let i = 0; i < data.length; i++) {
-          let d = data[i].user.gender;
-          let e = data[i].user.ethnicity;
-          // console.log(d)
-          // calculation for gender
-          if (d == 'male' && e == 'White') {
-            this.mw = this.mw + 1
-            console.log("male white:" + this.mw)
-          } else if (d == 'male' && e == 'Coloured') {
-            this.mc = this.mc + 1
-            console.log("male coloured:" + this.mc)
-          } else if (d == 'male' && e == 'Black') {
-            this.mb = this.mb + 1
-            console.log("maleblack:" + this.mb)
-          }
-          // females
-          if (d == 'female' && e == 'White') {
-            this.fw = this.fw + 1
-            console.log("f white:" + this.fw)
-          } else if (d == 'female' && e == 'Coloured') {
-            this.fc = this.fc + 1
-            console.log("f coloured:" + this.fc)
-          } else if (d == 'female' && e == 'Black') {
-            this.fb = this.fb + 1
-            console.log("f black:" + this.fb)
-          }
-          // graph
-          this.chart = new Chart('chart3', {
-            type: 'line',
-            data: {
-
-              labels: ['Black', 'White', 'Coloured'],
-              datasets: [{
-                // males
-                label: '# Male statistics per race',
-                data: [this.mb, this.mw, this.mc],
-
-                borderColor: [
-                  // 'rgba(255, 99, 132, 1)',
-                  // 'rgba(54, 162, 235, 1)',
-                  // 'rgba(255, 206, 86, 1)',
-                  // 'rgba(75, 192, 192, 1)',
-                  // 'rgba(153, 102, 255, 1)',
-                  'rgba(0,0,255, 1)'
-                  // rgb(0,0,255)
-                ],
-                borderWidth: 2
-              },
-              {
-                // females
-                label: '# female statistics per race',
-                data: [this.fb, this.fw, this.fc],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                  'rgba(255, 99, 132, 1)'
-                  // 'rgba(54, 162, 235, 1)',
-                  // 'rgba(255, 206, 86, 1)',
-                  // 'rgba(75, 192, 192, 1)',
-                  // 'rgba(153, 102, 255, 1)',
-                  // 'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 2
-              }
-
-              ],
-
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
-            }
-          });
-
-        }
-      }
-      )
+    }
+    )
 
 
 
-    })
+  })
   }
   initializeItems(): void {
     this.itemList = this.itemload;
@@ -410,7 +413,7 @@ export class HomeComponent implements OnInit {
   }
 
   Internertcafes() {
-
+    this.char()
     this.registereduser = false;
     this.registeredcafe = false;
     this.statistic = false;
@@ -434,7 +437,7 @@ this.search = true;
     this.search = false;
   }
   statistics() {
-
+    this.char()
     this.registereduser = false;
     this.registeredcafe = false;
     this.statistic = true;
@@ -443,6 +446,7 @@ this.search = true;
   }
   Home() {
     // this.home=true;
+    this.char()
     this.registereduser = true;
     this.registeredcafe = true;
     this.statistic = true;
